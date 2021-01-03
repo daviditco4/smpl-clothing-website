0843ecc867bb16f471b5cc773cdf5a058a1d5811
David Itcovici <daviditco4@gmail.com>
2021-01-02 21:04:51 -0300

    Create PWA project

---
+++ android/app/src/main/kotlin/com/example/smpl_clothing_website/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smpl_clothing_website
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SMPL Clothing',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Test')),
+      body: Container(),
+    );
+  }
+}
