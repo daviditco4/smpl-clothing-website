9fdfd63e33969943ebcad18123b71a2383425570
David Itcovici <daviditco4@gmail.com>
2021-01-15 00:30:49 -0300

    Create layout for the home page without shirts grid

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const brandYellow = Color(0xFFF1DC3A);
+    const brandBlack = Color(0xFF121212);
 
     return MaterialApp(
       title: 'SMPL Wear',
       theme: ThemeData(
         primaryColor: brandYellow,
+        shadowColor: brandBlack,
         fontFamily: 'Louis George Cafe',
+        textTheme: const TextTheme(
+          subtitle1: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 13.125,
+            letterSpacing: 0.45,
+            color: brandYellow,
+          ),
+          subtitle2: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 9.0,
+            letterSpacing: 13.5,
+            color: brandYellow,
+          ),
+          caption: TextStyle(
+            fontSize: 9.375,
+            letterSpacing: 0.45,
+            color: brandYellow,
+          ),
+          overline: TextStyle(
+            fontWeight: FontWeight.w300,
+            fontSize: 9.0,
+            letterSpacing: 4.5,
+            color: brandYellow,
+          ),
+        ),
+        iconTheme: const IconThemeData(size: 16.0, color: brandYellow),
       ),
       routes: {'/': (_) => HomePage()},
     );
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,111 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Scaffold(body: Container());
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    final subtitle1 = textTheme.subtitle1;
+
+    return Scaffold(
+      body: CarouselSlider(
+        options: CarouselOptions(
+          height: height,
+          viewportFraction: 1.0,
+          scrollDirection: Axis.vertical,
+          enableInfiniteScroll: false,
+        ),
+        items: [
+          Container(
+            height: height,
+            color: theme.shadowColor,
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'web/images/brand/logo.png',
+                  width: 350.0,
+                  height: 88.2,
+                ),
+                const SizedBox(height: 30.0),
+                Text('1ST DROP', style: textTheme.subtitle2),
+              ],
+            ),
+          ),
+          Container(
+            height: height,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(80.0),
+                    child: Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: [
+                        Image.asset(
+                          'web/images/brand/shirts_overview_background.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 90.0,
+                  color: theme.shadowColor,
+                  padding: const EdgeInsets.only(
+                    top: 15.0,
+                    left: 15.0,
+                    bottom: 10.0,
+                  ),
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('CONTACTANOS', style: subtitle1),
+                              const SizedBox(height: 7.5),
+                              Text(
+                                'contacto.sample.ar@gmail.com',
+                                style: textTheme.caption,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(width: 25.0),
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('REDES SOCIALES', style: subtitle1),
+                              const SizedBox(height: 4.0),
+                              const Icon(LineAwesomeIcons.instagram),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Text(
+                        'copyright SAMPLE - 2021 - todos los derechos reservados',
+                        style: textTheme.overline,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
